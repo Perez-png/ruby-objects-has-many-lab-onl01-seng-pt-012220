4355eab48ed8463052d4da3e189fcb900f0c7ab8
Xavier Perez <xperez007@gmail.com>
2020-02-20 14:53:45 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,22 @@
+require 'pry'
+
+class Song 
+  attr_accessor :name, :Prince 
+  @@all = []
+end
+
+def initialize()
+  @Prince = Prince
+  @@all << Prince
+
+def self.all 
+  @@all
+  
+def add_song(Kiss)
+    song.artist = self
+end
+
+end
+
+end
+end
